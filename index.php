bb0fd870f445b38dd73d07b5e282d1844670abb2
Loomeh <67561520+Loomeh@users.noreply.github.com>
2020-07-02 23:00:40 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -17,8 +17,10 @@ $m14 ="fusecavator is a nonce! DansGame";
 $m15 ="Crabbi is a nonce! DansGame";
 $m16 ="xsnith is a nonce! DansGame";
 $m16 ="xsnith is a nonce! DansGame";
+$m17 ="The California Raisins are nonces! DansGame";
+$m18 ="DylCat is a nonce! DansGame";
 
-$rm = array($m1, $m2, $m3, $m5, $m6, $m7, $m8, $m9, $m10, $m11, $m12, $m13, $m14, $m15, $m16);
+$rm = array($m1, $m2, $m3, $m5, $m6, $m7, $m8, $m9, $m10, $m11, $m12, $m13, $m14, $m15, $m16, $m17, $m18);
 
 $random_keys = array_rand($rm, 1);
 echo $rm[$random_keys];
