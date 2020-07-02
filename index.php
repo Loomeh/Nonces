770fd3eea42c67b03a94b043a1eeb60ee31d27c5
Loomeh <67561520+Loomeh@users.noreply.github.com>
2020-07-02 22:15:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+//Brobey Quotes Bot by Loomeh. You expectin' some copyright shit? nah lol.
+
+$m1 ="V3XXIE";
+$m2 ="Yayo";
+$m3 ="Onishima";
+$m5 ="Hayashi";
+$m6 ="JetSetStar";
+$m7 ="Naestrinus";
+$m8 ="kitcarsonn";
+$m9 ="mf_d00m";
+$m10 ="FingerQuick";
+$m11 ="yp";
+$m12 ="Brobey";
+$m13 ="7thAce";
+$m14 ="fusecavator";
+$m15 ="Crabbi";
+$m16 ="xsnith";
+
+$rm=array($m1, $m2, $m3, $m5, $m6, $m7, $m8, $m9, $m10, $m11, $m12, $m13, $m14, $m15, $m16)
+$random_keys = array_rand($rm, 1);
+echo $rm[$random_keys] + " is a nonce! DansGame";
+?>
