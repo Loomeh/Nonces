eba33bdd732bc0350aa012a204d41e5537eafd68
Loomeh <67561520+Loomeh@users.noreply.github.com>
2020-07-02 22:36:17 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php
 //Brobey Quotes Bot by Loomeh. You expectin' some copyright shit? nah lol.
 
-$m1 ="V3XXIE";
-$m2 ="Yayo";
-$m3 ="Onishima";
-$m5 ="Hayashi";
-$m6 ="JetSetStar";
-$m7 ="Naestrinus";
-$m8 ="kitcarsonn";
-$m9 ="mf_d00m";
-$m10 ="FingerQuick";
-$m11 ="yp";
-$m12 ="Brobey";
-$m13 ="7thAce";
-$m14 ="fusecavator";
-$m15 ="Crabbi";
-$m16 ="xsnith";
+$m1 ="V3XXIE is a nonce! DansGame";
+$m2 ="Yayo is a nonce! DansGame";
+$m3 ="Onishima is a nonce! DansGame";
+$m5 ="Hayashi is a nonce! DansGame";
+$m6 ="JetSetStar is a nonce! DansGame";
+$m7 ="Naestrinus is a nonce! DansGame";
+$m8 ="kitcarsonn is a nonce! DansGame";
+$m9 ="mf_d00m is a nonce! DansGame";
+$m10 ="FingerQuick is a nonce! DansGame";
+$m11 ="yp is a nonce! DansGame";
+$m12 ="Brobey is a nonce! DansGame";
+$m13 ="7thAce is a nonce! DansGame";
+$m14 ="fusecavator is a nonce! DansGame";
+$m15 ="Crabbi is a nonce! DansGame";
+$m16 ="xsnith is a nonce! DansGame";
+$m16 ="xsnith is a nonce! DansGame";
 
 $rm=array($m1, $m2, $m3, $m5, $m6, $m7, $m8, $m9, $m10, $m11, $m12, $m13, $m14, $m15, $m16);
 $random_keys = array_rand($rm, 1);
-gamer = $rm[random_keys];
-echo gamer + " is a nonce! DansGame";
+echo $rm[random_keys];
+
 ?>
