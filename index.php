ff28700e438fe2cc4a288980a3c87d905a6721d5
Loomeh <67561520+Loomeh@users.noreply.github.com>
2020-10-23 21:12:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $m1 ="V3XXIE is a nonce! DansGame";
 $m2 ="Yayo is a nonce! DansGame";
 $m3 ="Onishima is a nonce! DansGame";
 $m5 ="Hayashi is a nonce! DansGame";
-$m6 ="JetSetStar is a nonce! DansGame";
+$m6 ="Electrowolf is a nonce! DansGame";
 $m7 ="Naestrinus is a nonce! DansGame";
 $m8 ="kitcarsonn is a nonce! DansGame";
 $m9 ="mf_d00m is a nonce! DansGame";
@@ -19,8 +19,10 @@ $m16 ="xsnith is a nonce! DansGame";
 $m16 ="xsnith is a nonce! DansGame";
 $m17 ="The California Raisins are nonces! DansGame";
 $m18 ="DylCat is a nonce! DansGame";
+$m19 ="Gouji is a nonce! DansGame";
+$m20 ="antcan7000 is a nonce! DansGame";
 
-$rm = array($m1, $m2, $m3, $m5, $m6, $m7, $m8, $m9, $m10, $m11, $m12, $m13, $m14, $m15, $m16, $m17, $m18);
+$rm = array($m1, $m2, $m3, $m5, $m6, $m7, $m8, $m9, $m10, $m11, $m12, $m13, $m14, $m15, $m16, $m17, $m18, $m19, $m20);
 
 $random_keys = array_rand($rm, 1);
 echo $rm[$random_keys];
